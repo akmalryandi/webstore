335ddccfff54c77b57d5493d3ea8adc33c5d3691
akmalryandi <akmalryandii@gmail.com>
2025-08-19 09:47:14 +0700

    [FEAT] Product Collection Data

--- app/Livewire/ProductCatalog.php
+++ app/Livewire/ProductCatalog.php
@@ -2,16 +2,22 @@
 declare(strict_types=1);
 namespace App\Livewire;
 
+use App\Models\Tag;
 use App\Models\Product;
 use Livewire\Component;
 use App\Data\ProductData;
+use App\Data\ProductCollectionData;
 
 class ProductCatalog extends Component
 {
     public function render()
     {
+        $result_collection = Tag::query()->withType('collection')->withCount('products')->get();
+
         $result = Product::paginate(1); //ORM //Database Query
         $products = ProductData::collect($result);
-        return view('livewire.product-catalog', compact('products'));
+        $collection = ProductCollectionData::collect($result_collection);
+
+        return view('livewire.product-catalog', compact('products', 'collection'));
     }
 }
---
+++ app/Models/Tag.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\Tags\Tag as TagsTag;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+
+class Tag extends TagsTag
+{
+    // public function products()
+    // {
+    //     return $this->belongsToMany(Product::class);
+    // }
+
+    public function products(): MorphToMany
+    {
+        return $this->morphedByMany(Product::class, 'taggable');
+    }
+}
