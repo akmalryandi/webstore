83e41110e9a3588574cad90e5d3694da80e3e210
akmalryandi <akmalryandii@gmail.com>
2025-08-22 11:45:49 +0700

    [FEAT] Validation AddToCart

--- app/Livewire/AddToCart.php
+++ app/Livewire/AddToCart.php
@@ -23,9 +23,21 @@ class AddToCart extends Component
         $this->stock = $product->stock;
         $this->weight = $product->weight;
         $this->quantity = $cart->getItemBySku($product->sku)->quantity ?? 1;
+
+        $this->validate();
+    }
+
+    protected function rules() : array
+    {
+        return [
+            'quantity' => ['required', 'integer', 'min:1', "max:{$this->stock}"]
+        ];
     }
 
     public function addToCart(CartServiceInterface $cart){
+
+        $this->validate();
+
         $cart->addOrUpdate(new CartItemData(
             sku: $this->sku,
             quantity: $this->quantity,
