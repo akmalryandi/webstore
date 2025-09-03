01e6d3bb86dd6ecc394c5d049216948c4e12b692
akmalryandi <akmalryandii@gmail.com>
2025-09-03 11:00:04 +0700

    [FEAT] Preparing Region Data (Wilayah.id)

---
+++ app/Data/RegionData.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Data;
+
+use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Attributes\Computed;
+
+class RegionData extends Data
+{
+    #[Computed]
+    public string $label;
+    public function __construct(
+        public string $code,
+        public string $province,
+        public string $city,
+        public string $district,
+        public string $sub_distric,
+        public string $postal_code,
+        public string $country = 'indonesia'
+    ) {
+        $this->label = "$sub_distric, $district, $city, $province, $postal_code";
+    }
+}
