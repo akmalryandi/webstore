df295b4699b67d7f3d685adbf399bd307e946107
akmalryandi <akmalryandii@gmail.com>
2025-09-10 10:29:26 +0700

    [FEAT] Build Checkout Data

--- app/Livewire/Checkout.php
+++ app/Livewire/Checkout.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 
 use App\Data\CartData;
+use App\Data\CustomerData;
 use App\Models\Region;
 use App\Rules\ValidShippingHash;
 use App\Services\PaymentMethodQueryService;
@@ -15,6 +16,7 @@ use Illuminate\Support\Number;
 use App\Services\RegionQueryService;
 use Illuminate\Support\Facades\Gate;
 use App\Contract\CartServiceInterface;
+use App\Data\CheckoutData;
 use App\Rules\ValidPaymentMethodHash;
 use Spatie\LaravelData\DataCollection;
 use App\Services\ShippingMethodService;
@@ -186,9 +188,26 @@ class Checkout extends Component
     }
     public function placeAnOrder()
     {
-        $this->validate();
+        $validated = $this->validate();
 
-        dd($this->data);
+        $shipping_method = app(ShippingMethodService::class)->getShippingMethod(
+            data_get($validated, 'data.shipping_hash')
+        );
+
+        $payment_method = app(PaymentMethodQueryService::class)->getPaymentMethodByHash(
+            data_get($validated, 'data.payment_method_hash')
+        );
+
+        $checkout = CheckoutData::from([
+            'customer' => CustomerData::from(data_get($validated, 'data')),
+            'address_line' => data_get($validated, 'data.address_line'),
+            'origin' => $shipping_method->origin,
+            'destination' => $shipping_method->destination,
+            'cart' => $this->cart,
+            'shipping' => $shipping_method,
+            'payment' => $payment_method
+        ]);
+        dd($checkout);
     }
     public function render()
     {
