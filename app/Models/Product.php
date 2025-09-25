ab817f4a77e92c6a216f2ab815352a996e02be2e
akmalryandi <akmalryandii@gmail.com>
2025-09-25 10:44:36 +0700

    [FEAT] Activity Log Tracking

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,22 +2,29 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
-use Spatie\MediaLibrary\InteractsWithMedia;
-use Spatie\MediaLibrary\HasMedia;
-use Spatie\Image\Enums\Fit;
-use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Spatie\Tags\HasTags;
+use Spatie\Image\Enums\Fit;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\Activitylog\LogOptions;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class Product extends Model implements HasMedia
 {
-    use InteractsWithMedia, HasTags;
+    use InteractsWithMedia, HasTags, LogsActivity;
     public function registerMediaConversions(?Media $media = null): void
     {
         $this
             ->addMediaConversion('cover')
             ->fit(Fit::Contain, 300, 300)
             ->nonQueued();
+    }
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(['name', 'slug', 'stock']);
     }
 
 }
